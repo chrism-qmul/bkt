d525388f57ffcae8e02b78d981d49fc093466241
Chris Madge <chris@madg.es>
2022-01-29 18:15:25 +0000

    changed project groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bkt "0.1.0"
+(defproject net.clojars.chrism/bkt "0.1.0"
   :description "Bayesian Knowledge Tracing"
   :url "https://github.com/chrism-qmul/bkt"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
